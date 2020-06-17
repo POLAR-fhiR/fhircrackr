b4ee53e7f5892270ba17563311a02c304b89eccf
Jens <jens.przybilla@imise.uni-leipzig.de>
2020-06-17 15:57:58 +0200

    Shorten Example Code

--- examples/findMedication/example.R
+++ examples/findMedication/example.R
@@ -2,24 +2,9 @@
 #Additional all FHIR Patient Resources that are related to the MedicationStatement Resource will be searched.
 #This example works only at the hapi.fhir.org server.
 #start this example with
-#Rscript example.R -o result
-#last change: 2020_06_16
+# Rscript example.R
 
-#devtools::install_github("POLAR-fhiR/fhiR", quiet = T)
 rm(list = ls( ))
-out_dir <- "result"
-
-endpoint <- "https://hapi.fhir.org/baseR4"
-
-fhir_search_request  <- paste0(
-	"MedicationStatement?",
-	"code=http://snomed.info/ct|429374003",
-	"&_include=MedicationStatement:subject",
-	"&_format=xml",
-	"&_pretty=true",
-	"&_count=500000")
-
-url <- fhiR::paste_paths(path1 = endpoint, path2 = fhir_search_request)
 
 tables_design <- list(
 	MedikationStatement = list(
@@ -28,9 +13,9 @@ tables_design <- list(
 			AID = "id/@value",
 			STATUS.TEXT ="text/status/@value",
 			STATUS = "status/@value",
-			MEDIKATION.SYSTEM  = "medicationCodeableConcept/coding/system/@value",
-			MEDIKATION.CODE    = "medicationCodeableConcept/coding/code/@value",
-			MEDIKATION.ANZEIGE = "medicationCodeableConcept/coding/display/@value",
+			MEDICATION.SYSTEM  = "medicationCodeableConcept/coding/system/@value",
+			MEDICATION.CODE    = "medicationCodeableConcept/coding/code/@value",
+			MEDICATION.DISPLAY = "medicationCodeableConcept/coding/display/@value",
 			DOSAGE  = "dosage/text/@value",
 			PATIENT = "subject/reference/@value",
 			LAST.UPDATE  = "meta/lastUpdated/@value"
@@ -39,39 +24,32 @@ tables_design <- list(
 	Patients = list(
 		".//Patient",
 		list( 
-			PID             = "id/@value", 
-			NAME.VERWENDUNG = "name/use/@value", 
-			VORNAME         = "name/given/@value", 
-			NACHNAME        = "name/family/@value",
-			GESCHLECHT      = "gender/@value", 
-			GEBURTSTAG      = "birthDate/@value" 
+			PID           = "id/@value", 
+			NAME.USE      = "name/use/@value", 
+			NAME.GIVEN    = "name/given/@value", 
+			NAME.FAMILY   = "name/family/@value",
+			SEX           = "gender/@value", 
+			BIRTHDAY       = "birthDate/@value" 
 		)
 	)
 )
 
+url  <- paste0(
+  "https://hapi.fhir.org/baseR4/",
+  "MedicationStatement?",
+  "code=http://snomed.info/ct|429374003",
+  "&_include=MedicationStatement:subject")
 
-bundles <- fhiR::get_bundle(request = url, verbose = T)
+bundles <- fhiR::fhir_search(url)
 
-list_of_tables <- fhiR::bundle2dfs(bundle = bundles, design = tables_design)
-
-
-if(! dir.exists(out_dir)) {
-  
-  cat(paste0("   - create directory: ", out_dir, "...\n"))
-  
-  dir.create(out_dir, recursive = T)
-  
-} else cat(paste0("   - directory ", out_dir, " already exists. do nothing...\n"))
-
-cat("   - write csv tables...\n")
+list_of_tables <- fhiR::fhir2dfs(bundle = bundles, design = tables_design)
 
 for(n in names(list_of_tables)) {
 	
-	write.table(list_of_tables[[ n ]], file = paste0( "result/", n, ".csv" ), na = "", sep = ";", dec = ".", row.names = F, quote=F)
+	write.table(list_of_tables[[ n ]], file = paste0(n, ".csv"), na = "", sep = ";", dec = ".", row.names = F, quote=F)
 }
 
-cat("   - write tables.RData...\n")
-save(list_of_tables, file = "result/tables.RData")
+save(list_of_tables, file = "tables.RData")
 cat("   - finish\n")
 
 
--- examples/findPatients/example.R
+++ examples/findPatients/example.R
@@ -1,60 +1,35 @@
 #This example searches for all FHIR Resources:Patient 
 #This example works only at the hapi.fhir.org server.
 #start this example with
-#Rscript example.R -o result
-#last change: 2020_06_16
+#Rscript example.R 
 
-#devtools::install_github("POLAR-fhiR/fhiR", quiet = T)
 rm( list = ls( ) )
-endpoint <- "https://hapi.fhir.org/baseR4"
-
-fhir_search_request  <- paste0(
-	"Patient?",
-	"gender=female",
-	"&_format=xml",
-	"&_pretty=true",
-	"&_count=500000")
-
-url <- fhiR::paste_paths(path1 = endpoint, path2 = fhir_search_request)
 
 tables_design <- list(
 	Patients = list(
 		".//Patient",
 		list( 
-			PID             = "id/@value", 
-			NAME.VERWENDUNG = "name/use/@value", 
-			VORNAME         = "name/given/@value", 
-			NACHNAME        = "name/family/@value",
-			GESCHLECHT      = "gender/@value", 
-			GEBURTSTAG      = "birthDate/@value" 
+			PID           = "id/@value", 
+			NAME.USE      = "name/use/@value", 
+			NAME.GIVEN    = "name/given/@value", 
+			NAME.FAMILY   = "name/family/@value",
+			SEX           = "gender/@value", 
+			BIRTHDAY      = "birthDate/@value" 
 		)
 	)
 )
 
 
-bundles <- fhiR::get_bundle(request = url, verbose = T)
+bundles <- fhiR::fhir_search("https://hapi.fhir.org/baseR4/Patient?", max.bundles=3)
 
-list_of_tables <- fhiR::bundle2dfs(bundle = bundles, design = tables_design)
-
-out_dir <- "result"
-
-if(! dir.exists(out_dir)) {
-  
-  cat(paste0("   - create directory: ", out_dir, "...\n"))
-  
-  dir.create(out_dir, recursive = T)
-  
-} else cat(paste0("   - directory ", out_dir, " already exists. do nothing...\n"))
-
-cat("   - write csv tables...\n")
+list_of_tables <- fhiR::fhir2dfs(bundle = bundles, design = tables_design)
 
 for(n in names(list_of_tables)) {
 	
-	write.table(list_of_tables[[ n ]], file = paste0( "result/",n, ".csv" ), na = "", sep = ";", dec = ".", row.names = F, quote=F)
+	write.table(list_of_tables[[ n ]], file = paste0(n, ".csv"), na = "", sep = ";", dec = ".", row.names = F, quote=F)
 }
 
-cat("   - write tables.RData...\n")
-save(list_of_tables, file = "result/tables.RData")
+save(list_of_tables, file = "tables.RData")
 cat("   - finish\n")
 
 
