dbe7306343f8015ad919610508ce490f2b7a3f5c
Jens <jens.przybilla@imise.uni-leipzig.de>
2020-06-18 14:29:01 +0200

    Rename examples to demo

--- demo/findMedication_1.R
+++ demo/findMedication_1.R
@@ -1,8 +1,7 @@
 #This example use fhir_search to find all FHIR Resources:MedicationStatement with snomed.code 429374003 (Simvastatin 40mg)
 #Additional all FHIR Patient Resources that are related to the MedicationStatement Resource will be searched.
 #This example works only at the hapi.fhir.org server.
-#start this example with
-# Rscript example.R
+
 
 tables_design <- list(
 	MedikationStatement = list(
@@ -21,13 +20,13 @@ tables_design <- list(
 	),
 	Patients = list(
 		".//Patient",
-		list( 
-			PID           = "id/@value", 
-			NAME.USE      = "name/use/@value", 
-			NAME.GIVEN    = "name/given/@value", 
+		list(
+			PID           = "id/@value",
+			NAME.USE      = "name/use/@value",
+			NAME.GIVEN    = "name/given/@value",
 			NAME.FAMILY   = "name/family/@value",
-			SEX           = "gender/@value", 
-			BIRTHDAY       = "birthDate/@value" 
+			SEX           = "gender/@value",
+			BIRTHDAY      = "birthDate/@value"
 		)
 	)
 )
@@ -41,9 +40,9 @@ url  <- paste0(
 bundles <- fhiR::fhir_search(url)
 
 list_of_tables <- fhiR::fhir2dfs(bundle = bundles, design = tables_design)
-
+cat("   - write tables as csv and Rdata files to disk \n")
 for(n in names(list_of_tables)) {
-	
+
 	write.table(list_of_tables[[ n ]], file = paste0(n, ".csv"), na = "", sep = ";", dec = ".", row.names = F, quote=F)
 }
 
--- demo/findPatients_1.R
+++ demo/findPatients_1.R
@@ -1,18 +1,17 @@
-#This example searches for all FHIR Resources:Patient 
+#This example searches for all FHIR Resources:Patient
 #This example works only at the hapi.fhir.org server.
-#start this example with
-#Rscript example.R 
+
 
 tables_design <- list(
 	Patients = list(
 		".//Patient",
-		list( 
-			PID           = "id/@value", 
-			NAME.USE      = "name/use/@value", 
-			NAME.GIVEN    = "name/given/@value", 
+		list(
+			PID           = "id/@value",
+			NAME.USE      = "name/use/@value",
+			NAME.GIVEN    = "name/given/@value",
 			NAME.FAMILY   = "name/family/@value",
-			SEX           = "gender/@value", 
-			BIRTHDAY      = "birthDate/@value" 
+			SEX           = "gender/@value",
+			BIRTHDAY      = "birthDate/@value"
 		)
 	)
 )
@@ -21,9 +20,9 @@ tables_design <- list(
 bundles <- fhiR::fhir_search("https://hapi.fhir.org/baseR4/Patient?", max.bundles=3)
 
 list_of_tables <- fhiR::fhir2dfs(bundle = bundles, design = tables_design)
-
+cat("   - write tables as csv and Rdata files to disk \n")
 for(n in names(list_of_tables)) {
-	
+
 	write.table(list_of_tables[[ n ]], file = paste0(n, ".csv"), na = "", sep = ";", dec = ".", row.names = F, quote=F)
 }
 
