39a52021bdd4b044dfb53430342f7290ce5f3aa5
Julia Gantner <julia.gantner@med.uni-jena.de>
2020-08-14 12:35:12 +0200

    format

--- R/design.R
+++ R/design.R
@@ -1,32 +1,72 @@
 fixx <- function (list, names, defaults=NULL) {
-	if (is.null(list)) warning("Argument list is NULL") else if (! is.list(list)) warning(paste0("Argument list is ", typeof(list), " but must be list or NULL."))
-	if (length(list) < length(names)) list <- append(list, lapply(seq_len(length(names) - length(list)), function (x) NULL))
-	lnames <- names(list)
-	if (is.null(lnames)) names(list) <- names
-	else {
-		wnames <- setdiff(setdiff(lnames, names), "")
-		if (0 < length(wnames)) {
-			stop(paste0("Unknown Argument(s) ", wnames, "."))
+
+	if (is.null(list)) {
+
+			warning("Argument list is NULL")
+
+		}else if (!is.list(list)) {
+
+			warning(paste0("Argument list is ", typeof(list), " but must be list or NULL."))
+
 		}
+
+	if (length(list) < length(names)){
+
+		list <- append(list, lapply(seq_len(length(names) - length(list)), function (x) NULL))
+	}
+
+	lnames <- names(list)
+
+	if (is.null(lnames)) {
+
+		names(list) <- names
+
+	}else{
+
+		wnames <- setdiff(setdiff(lnames, names), "")
+
+		if (0 < length(wnames)) {
+
+			stop(paste0("Unknown Argument(s) ", wnames, "."))
+
+		}
+
 		lnames[lnames == ""] <- setdiff(names, lnames)[seq_len(sum(lnames == ""))]
 		names(list) <- lnames
 		list <- list[names]
 	}
-	if (! is.null(defaults)) for (i in seq_along(list)) if (is.null(list[[i]])) list[[i]] <- defaults[[i]]
+
+	if (! is.null(defaults)){
+
+		for (i in seq_along(list)){
+
+			if (is.null(list[[i]])) list[[i]] <- defaults[[i]]
+		}
+	}
 	list
 }
 
 fix <- function (design) {
 	design <- fixx(list = design, names = c("Resource", "Columns", "Decoration"))
-	design$Decoration <- if (is.null(design$Decoration)) NULL else fixx(design$Decoration, c("Separator", "Brackets", "Remove_Empty_Columns"), defaults = list(" ", c("<", ">"), TRUE))
+	design$Decoration <- if (is.null(design$Decoration)){
+
+		NULL
+
+	}else{
+
+		fixx(design$Decoration, c("Separator", "Brackets", "Remove_Empty_Columns"), defaults = list(" ", c("<", ">"), TRUE))
+
+	}
+
 	design
 }
 
 is_valid_design <- function (design) {
 	d <- fix(design)
-	if (! is.character(d$Resource)) stop(paste0("Resource of Design is ", typeof(d$Resource), " but must be character."))
-	if (! is.null(d$Columns) && ! is.character(d$Columns) && ! is.list(d$Columns)) warning(paste0("Columns of Design is ", typeof(d$Columns), " but must be character, list or NULL."))
-	if (! is.null(d$Decoration) && ! is.list(d$Decoration)) warning(paste0("Decoration of Design is ", typeof(d$Decoration), " but must be list or NULL."))
+
+	if (!is.character(d$Resource)) stop(paste0("Resource of Design is ", typeof(d$Resource), " but must be character."))
+	if (!is.null(d$Columns) && ! is.character(d$Columns) && ! is.list(d$Columns)) warning(paste0("Columns of Design is ", typeof(d$Columns), " but must be character, list or NULL."))
+	if (!is.null(d$Decoration) && ! is.list(d$Decoration)) warning(paste0("Decoration of Design is ", typeof(d$Decoration), " but must be list or NULL."))
 	if (is.list(d$Decoration)) {
 		d <- d$Decoration
 		if (! is.null(d$Separator) && ! is.character(d$Separator)) stop(paste0("Separator of Design's Decoration is ", typeof(d$Resource), " but must be character or NULL."))
