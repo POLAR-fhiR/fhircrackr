5aeec9a84aacb3d55ded006422fe25d58b143f14
Julia Gantner <julia.gantner@med.uni-jena.de>
2020-08-14 14:52:10 +0200

    rename stuff, separate valid df description from valid design tests

--- R/design.R
+++ R/design.R
@@ -1,4 +1,6 @@
-fixx <- function (list, names, defaults=NULL) {
+
+#Fix list by assigning proper names and defaults
+fix <- function (list, names, defaults=NULL) {
 
 	if (is.null(list)) {
 
@@ -27,7 +29,8 @@ fixx <- function (list, names, defaults=NULL) {
 
 		if (0 < length(wnames)) {
 
-			stop(paste0("Unknown Argument(s) ", wnames, "."))
+			stop(paste0("Unknown design component ", wnames,
+						". Names of design components can only be resource, cols, style, sep, brackets and rm_empty_cols\n"))
 
 		}
 
@@ -46,120 +49,236 @@ fixx <- function (list, names, defaults=NULL) {
 	list
 }
 
-fix <- function (design) {
-	design <- fixx(list = design, names = c("Resource", "Columns", "Decoration"))
-	design$Decoration <- if (is.null(design$Decoration)){
+
+#fix df description
+fix_df_desc <- function (df_desc) {
+
+	df_desc <- fix(list = df_desc, names = c("resource", "cols", "style"))
+
+	df_desc$style <- if (is.null(df_desc$style)){
 
 		NULL
 
 	}else{
 
-		fixx(design$Decoration, c("Separator", "Brackets", "Remove_Empty_Columns"), defaults = list(" ", c("<", ">"), TRUE))
+		fix(df_desc$style, c("sep", "brackets", "rm_empty_cols"), defaults = list(" ", c("<", ">"), TRUE))
 
 	}
 
-	design
+	df_desc
 }
 
-is_valid_design <- function (design) {
-	d <- fix(design)
 
-	if (!is.character(d$Resource)) stop(paste0("Resource of Design is ", typeof(d$Resource), " but must be character."))
-	if (!is.null(d$Columns) && ! is.character(d$Columns) && ! is.list(d$Columns)) warning(paste0("Columns of Design is ", typeof(d$Columns), " but must be character, list or NULL."))
-	if (!is.null(d$Decoration) && ! is.list(d$Decoration)) warning(paste0("Decoration of Design is ", typeof(d$Decoration), " but must be list or NULL."))
-	if (is.list(d$Decoration)) {
-		d <- d$Decoration
-		if (! is.null(d$Separator) && ! is.character(d$Separator)) stop(paste0("Separator of Design's Decoration is ", typeof(d$Resource), " but must be character or NULL."))
-		if (! is.null(d$Brackets) && ! is.character(d$Brackets) ) stop(paste0("Brackets of Design's Decoration is ", typeof(d$Brackets), " but must be character or list."))
-		if (! is.null(d$Remove_Empty_Columns) && ! is.logical(d$Remove_Empty_Columns)) stop(paste0("Remove_Empty_Columns of Design's Decoration is ", typeof(d$Remove_Empty_Columns), " but must be logical or NULL."))
+#check df_description
+is_valid_df_desc <- function (df_desc) {
+
+	#TODO: Hier noch andere Checks aus is_invalid_design aus helpers.R ergänzen (zB xml-Ausdrücke checken)
+
+	d <- fix_df_desc(df_desc)
+
+	if (!is.character(d$resource)) {
+		message <- paste0("resource component of data.frame description is ", typeof(d$Resource), " but must be character.")
+		return(data.frame(valid=FALSE, message))
 	}
-	TRUE
+
+	if (!is.null(d$cols) && !is.character(d$cols) && !is.list(d$cols)){
+		message <- paste0("cols component of data.frame description  is ", typeof(d$Columns), " but must be character, list or NULL.")
+		return(data.frame(valid=FALSE, message))
+	}
+
+	if (!is.null(d$style) && ! is.list(d$style)){
+		message <-paste0("style component of data.frame description is ", typeof(d$Decoration), " but must be list or NULL.")
+		return(data.frame(valid=FALSE, message))
+	}
+
+	if (is.list(d$style)) {
+		d <- d$style
+		if (!is.null(d$sep) && ! is.character(d$sep)) {
+			message <-paste0("sep element of style component is ", typeof(d$Resource), " but must be character or NULL.")
+			return(data.frame(valid=FALSE, message))
+		}
+
+		if (!is.null(d$brackets) && ! is.character(d$brackets)){
+			message <-paste0("brackets element of style component is ", typeof(d$Brackets), " but must be character or list.")
+			return(data.frame(valid=FALSE, message))
+		}
+		if (!is.null(d$rm_empty_cols) && ! is.logical(d$rm_empty_cols)){
+			message <-paste0("rm_empty_cols element of style component is ", typeof(d$Remove_Empty_Columns), " but must be logical or NULL.")
+			return(data.frame(valid=FALSE, message))
+		}
+	}
+
+	data.frame(valid=TRUE, message="fine")
 }
 
 
-is_valid_design(
-	design = list(
+
+#check design
+is_valid_design <- function(design){
+
+	#TODO: Hier noch andere Checks aus is_invalid_design aus helpers.R ergänzen
+
+	#general checks
+	if (is.null(designs)) {
+		warning("Argument design is NULL")
+		return(FALSE)
+	}
+
+	if (!is.list(designs)) {
+		warning("Argument design has to be a list")
+		return(FALSE)
+	}
+
+	if (length(designs) < 1) {
+		warning("Argument design has length 0")
+		return(FALSE)
+	}
+
+	#checks of df_descriptions
+	df_descr_results <- plyr::ldply(design, is_valid_df_desc)
+
+	df_descr_results$number <- 1:nrow(df_descr_results)
+
+	invalid <- df_descr_results[!df_descr_results$valid,]
+
+	if(nrow(invalid)>0){
+		warning(
+			"The following data.frame descriptions in you design seem to be invalid:\n",
+			paste0("Data.frame description no.", invalid$number, " (", invalid$.id,")"," : ", invalid$message, "\n")
+		)
+		return(FALSE)
+	}
+
+	return(TRUE)
+
+}
+
+
+
+
+###Tests
+
+#richtig mit Namen
+design1 <- list(
+	list(
+		resource = "//Patient",
+		cols = list(
+			ID = "id"
+		),
+		style= list(
+			" ",
+			rm_empty_cols = FALSE
+		)
+	)
+)
+
+is_valid_design(design1)
+
+#Falsche Namen: wirft momentan noch Fehler: In Warnung umwandeln und als invalid markieren?
+design2 <- list(
+	list(
+		Resource = "//Patient",
+		cols = list(
+			ID = "id"
+		),
+		stylE= list(
+			" ",
+			rm_empty_cols = FALSE
+		)
+	)
+)
+
+is_valid_design(design2)
+
+#richtig ohne Namen
+design3 <- list(
+	list(
 		"//Patient",
 		list(
 			ID = "id"
 		),
 		list(
 			" ",
-			Remove_Empty_Columns = FALSE
+			rm_empty_cols = FALSE
 		)
 	)
 )
 
-(f <- fix(
-	design = list(
-		Decoration = list(
-			Remove_Empty_Columns = FALSE,
-			Brackets = c('[', ']'),
+is_valid_design(design3)
+
+
+#fix df description with wrong order
+(f <- fix_df_desc(
+	df_desc = list(
+		style = list(
+			rm_empty_cols = FALSE,
+			brackets = c('[', ']'),
 			" >> "
 		),
 		"//Patient",
-		Columns = list(
+		cols = list(
 			ID = "id"
 		)
 	)
 ))
 
-designs = list(
+#long design mit zwei Fehlern:
+design <- list(
 	Pat = list(
-		Resource = "//Patient"
+		resource = NULL
 	),
 	Pat = list(
-		Resource = "//Patient",
-		Columns = ".//*"
+		resource = "//Patient",
+		cols = ".//*"
 	),
 	Pat = list(
-		Resource = "//Patient",
-		Columns  = list(
+		resource = "//Patient",
+		cols  = list(
 			ID = "id"
 		)
 	),
 	Pat = list(
-		Resource = "//Patient",
-		Columns = list(
+		resource = 7,
+		cols = list(
 			ID = "id"
 		),
-		Decoration = list(
-			Separator = " ",
-			Brackets = c("[", "]"),
-			Remove_Empty_Columns = TRUE
+		style = list(
+			sep = " ",
+			brackets = c("[", "]"),
+			rm_empty_cols = TRUE
 		)
 	),
 	Pat = list(
-		Resource = "//Patient",
-		Columns = ".//*",
-		Decoration = list(
-			Separator = " ",
-			Brackets = c("[", "]"),
-			Remove_Empty_Columns = TRUE
+		resource = "//Patient",
+		cols = ".//*",
+		style = list(
+			sep = " ",
+			brackets = c("[", "]"),
+			rm_empty_cols = TRUE
 		)
 	),
 	Pat = list(
-		Resource = "//Patient",
-		Columns = NULL, # wuerde man nicht machen
-		Decoration = list(
-			Separator = " ",
-			Brackets = c("[", "]"),
-			Remove_Empty_Columns = TRUE
+		resource = "//Patient",
+		cols = NULL, # wuerde man nicht machen
+		style = list(
+			sep = " ",
+			brackets = c("[", "]"),
+			rm_empty_cols = TRUE
 		)
 	),
 	Pat = list(
-		Resource = "//Patient",
-		Columns = NULL, # wuerde man nicht machen
-		Decoration = list(
-			Separator = " ",
-			Brackets = "#"
+		resource = "//Patient",
+		cols = NULL, # wuerde man nicht machen
+		style = list(
+			sep = " ",
+			brackets = "#"
 		)
 	),
 	Pat = list(
-		Resource = "//Patient",
-		Columns = NULL, # wuerde man nicht machen
+		resource = "//Patient",
+		cols = NULL, # wuerde man nicht machen
 		list(
-			Separator = " "
+			sep = " "
 		)
 	),
 	Pat = list(
@@ -171,9 +290,9 @@ designs = list(
 		)
 	),
 	Pat = list(
-		Resource = "//Patient",
-		Decoration = list(
-			Remove_Empty_Columns = TRUE
+		resource = "//Patient",
+		style = list(
+			rm_empty_cols = TRUE
 		)
 	),
 	Pat = list(
@@ -250,6 +369,11 @@ designs = list(
 		)
 	)
 )
+
+is_valid_design(design)
+
+
+
 assign("last.warning", NULL, envir = baseenv())
 (designs_fixed <- lapply(designs, fix))
 warnings()
