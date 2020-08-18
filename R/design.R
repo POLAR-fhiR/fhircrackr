bc5c6808278d63fac03af58e7163499ec216f895
Unknown <tpeschel@life.uni-leipzig.de>
2020-08-18 20:33:03 +0200

    Intermezzo

--- R/design.R
+++ R/design.R
@@ -1,4 +1,4 @@
-#rm(list = ls())
+rm(list = ls())
 
 #Fix list by assigning proper names and defaults
 fix <- function (list, names, defaults=NULL) {
@@ -75,43 +75,21 @@ is_valid_df_desc <- function (df_desc) {
 
 	#dbg df_desc <- design[[1]]
 
-	d <- fix_df_desc(df_desc = df_desc)
-	testbundle <- xml2::read_xml("<Bundle>   </Bundle>")
+	#TODO: Hier noch andere Checks aus is_invalid_design aus helpers.R ergänzen (zB xml-Ausdrücke checken)
 
-	#check resource
+	d <- fix_df_desc(df_desc = df_desc)
+
 	if (!is.character(d$resource)) {
+
 		message <- paste0("resource component of data.frame description is ", typeof(d$resource), " but must be character.")
 		return(data.frame(valid=FALSE, message))
 	}
 
-	if(length(d$resource) != 1) {
-		message <- paste0("resource component of data.frame description has length ", length(d$resource), " but should be of length 1.")
-		return(data.frame(valid=FALSE, message))
-
-	}
-
-	#check columns
 	if (!is.null(d$cols) && !is.character(d$cols) && !is.list(d$cols)){
-		message <- paste0("cols component of data.frame description is ", typeof(d$cols), " but must be character, list or NULL.")
+		message <- paste0("cols component of data.frame description  is ", typeof(d$cols), " but must be character, list or NULL.")
 		return(data.frame(valid=FALSE, message))
 	}
 
-	expressions <- unlist(df_desc$cols)
-
-	for (i in seq_along(expressions)) {
-		out <- tryCatch(
-			xml2::xml_find_all(testbundle, expressions[[i]]),
-			warning = function(x) {
-					if (grepl("Invalid expression", x))
-					message <- 	paste("One of the strings you have provided in the cols component is not a valid XPath expression. Please revise the following expression: ",
-										esc(expressions[[i]]))
-					return(data.frame(valid=FALSE, message))
-			}
-		)
-		return(out)
-	}
-
-	#check style
 	if (!is.null(d$style) && ! is.list(d$style)) {
 		message <- paste0("style component of data.frame description is ", typeof(d$style), " but must be list or NULL.")
 		return(data.frame(valid=FALSE, message))
@@ -129,16 +107,9 @@ is_valid_df_desc <- function (df_desc) {
 
 		if (!is.null(d$brackets) && ! is.character(d$brackets)) {
 
-			message <- paste0("brackets element of style component is ", typeof(d$brackets), " but must be character.")
+			message <- paste0("brackets element of style component is ", typeof(d$brackets), " but must be character or list.")
 			return(data.frame(valid=FALSE, message))
 		}
-
-		if (!is.null(d$brackets) && length(brackets)!=2) {
-
-			message <- paste0("brackets element of style component has length", length(d$brackets), " but must be of length 2.")
-			return(data.frame(valid=FALSE, message))
-		}
-
 		if (!is.null(d$rm_empty_cols) && ! is.logical(d$rm_empty_cols)) {
 			message <- paste0("rm_empty_cols element of style component is ", typeof(d$rm_empty_cols), " but must be logical or NULL.")
 			return(data.frame(valid=FALSE, message))
@@ -153,6 +124,8 @@ is_valid_df_desc <- function (df_desc) {
 #check design
 is_valid_design <- function(design){
 
+	#TODO: Hier noch andere Checks aus is_invalid_design aus helpers.R ergänzen
+
 	#general checks
 	if (is.null(design)) {
 		warning("Argument design is NULL")
@@ -161,11 +134,6 @@ is_valid_design <- function(design){
 
 	if (!is.list(design)) {
 		warning("Argument design has to be a list")
-		return(FALSE)
-	}
-
-	if (is.null(names(design)) || any(names(design) == "")) {
-		warning("Argument design should be a named list of data.frame descriptions, but at least one of the elements of design is unnamed.")
 		return(FALSE)
 	}
 
@@ -183,7 +151,7 @@ is_valid_design <- function(design){
 
 	if (0 < nrow(invalid)){
 		warning(
-			"The following data.frame descriptions in your design seem to be invalid:\n",
+			"The following data.frame descriptions in you design seem to be invalid:\n",
 			paste0("Data.frame description no.", invalid$number, " (", invalid$.id,")"," : ", invalid$message, "\n")
 		)
 		return(FALSE)
@@ -194,228 +162,292 @@ is_valid_design <- function(design){
 
 
 
-#
-# ###Tests
-#
-# #richtig mit Namen
-# design1 <- list(
-# 	Pat = list(
-# 		Resource = "//Patient",
-# 		cols = list(
-# 			ID = "id"
-# 		),
-# 		style= list(
-# 			" ",
-# 			rm_empty_cols = FALSE
-# 		)
-# 	)
-# )
-#
-# is_valid_design(design = design1)
-#
-# #Falsche Namen: wirft momentan noch Fehler: In Warnung umwandeln und als invalid markieren?
-# design2 <- list(
-# 	Pat = list(
-# 		resource = "//Patient",
-# 		cols = list(
-# 			ID = "id"
-# 		),
-# 		style = list(
-# 			" ",
-# 			rm_empty_cols = FALSE
-# 		)
-# 	)
-# )
-#
-# is_valid_design(design2)
-#
-# #richtig ohne Namen
-# design3 <- list(
-# 	list(
-# 		"//Patient",
-# 		list(
-# 			ID = "id"
-# 		),
-# 		list(
-# 			" ",
-# 			rm_empty_cols = FALSE
-# 		)
-# 	)
-# )
-#
-# is_valid_design(design3)
-#
-#
-# #fix df description with wrong order
-# (f <- fix_df_desc(
-# 	df_desc = list(
-# 		style = list(
-# 			rm_empty_cols = FALSE,
-# 			brackets = c('[', ']'),
-# 			" >> "
-# 		),
-# 		"//Patient",
-# 		cols = list(
-# 			ID = "id"
-# 		)
-# 	)
-# ))
-#
-# #long design mit zwei Fehlern:
-# design <- list(
-# 	Pat = list(
-# 		resource = NULL
-# 	),
-# 	Pat = list(
-# 		resource = "//Patient",
-# 		cols = ".//*"
-# 	),
-# 	Pat = list(
-# 		resource = "//Patient",
-# 		cols  = list(
-# 			ID = "id"
-# 		)
-# 	),
-# 	Pat = list(
-# 		resource = 7,
-# 		cols = list(
-# 			ID = "id"
-# 		),
-# 		style = list(
-# 			sep = " ",
-# 			brackets = c("[", "]"),
-# 			rm_empty_cols = TRUE
-# 		)
-# 	),
-# 	Pat = list(
-# 		resource = "//Patient",
-# 		cols = ".//*",
-# 		style = list(
-# 			sep = " ",
-# 			brackets = c("[", "]"),
-# 			rm_empty_cols = TRUE
-# 		)
-# 	),
-# 	Pat = list(
-# 		resource = "//Patient",
-# 		cols = NULL, # wuerde man nicht machen
-# 		style = list(
-# 			sep = " ",
-# 			brackets = c("[", "]"),
-# 			rm_empty_cols = TRUE
-# 		)
-# 	),
-# 	Pat = list(
-# 		resource = "//Patient",
-# 		cols = NULL, # wuerde man nicht machen
-# 		style = list(
-# 			sep = " ",
-# 			brackets = "#"
-# 		)
-# 	),
-# 	Pat = list(
-# 		resource = "//Patient",
-# 		cols = NULL, # wuerde man nicht machen
-# 		list(
-# 			sep = " "
-# 		)
-# 	),
-# 	Pat = list(
-# 		"//Patient",
-# 		NULL,
-# 		list(
-# 			NULL,
-# 			"#"
-# 		)
-# 	),
-# 	Pat = list(
-# 		resource = "//Patient",
-# 		style = list(
-# 			rm_empty_cols = TRUE
-# 		)
-# 	),
-# 	Pat = list(
-# 		"//Patient"
-# 	),
-# 	Pat = list(
-# 		"//Patient",
-# 		".//*"
-# 	),
-# 	Pat = list(
-# 		"//Patient",
-# 		list(
-# 			ID = "id"
-# 		)
-# 	),
-# 	Pat = list(
-# 		"//Patient",
-# 		list(
-# 			ID = "id"
-# 		),
-# 		list(
-# 			" ",
-# 			c("[", "]"),
-# 			TRUE
-# 		)
-# 	),
-# 	Pat = list(
-# 		"//Patient",
-# 		".//*",
-# 		list(
-# 			" ",
-# 			c("[", "]"),
-# 			TRUE
-# 		)
-# 	),
-# 	Pat = list(
-# 		"//Patient",
-# 		NULL,
-# 		list(
-# 			" ",
-# 			c("[", "]"),
-# 			TRUE
-# 		)
-# 	),
-# 	Pat = list(
-# 		"//Patient",
-# 		NULL,
-# 		list(
-# 			" ",
-# 			"#"
-# 		)
-# 	),
-# 	Pat = list(
-# 		"//Patient",
-# 		NULL,
-# 		list(
-# 			" "
-# 		)
-# 	),
-# 	Pat = list(
-# 		"//Patient",
-# 		NULL,
-# 		list(
-#
-# 		)
-# 	),
-# 	Pat = list(
-# 		"//Patient",
-# 		NULL,
-# 		list(
-# 			NULL,
-# 			NULL,
-# 			TRUE
-# 		)
-# 	)
-# )
-#
-# is_valid_design(design)
-#
-#
-#
-# assign("last.warning", NULL, envir = baseenv())
-# (designs_fixed <- lapply(designs, fix))
-# warnings()
-# (is_valid_Design <- all(sapply(designs, is_valid_design)))
-# warnings()
-#
+
+###Tests
+
+#richtig mit Namen
+design1 <- list(
+	Pat = list(
+		Resource = "//Patient",
+		cols = list(
+			ID = "id"
+		),
+		style= list(
+			" ",
+			rm_empty_cols = FALSE
+		)
+	)
+)
+
+is_valid_design(design = design1)
+
+#Falsche Namen: wirft momentan noch Fehler: In Warnung umwandeln und als invalid markieren?
+design2 <- list(
+	Patient = list(
+		resource = "//Patient",
+		cols = list(
+			ID = "id",
+			VName = "name/given",
+			NName = "name/family"
+		),
+		style = list(
+			" ",
+			rm_empty_cols = FALSE
+		)
+	)
+)
+
+is_valid_design(design2)
+
+#richtig ohne Namen
+design3 <- list(
+	list(
+		"//Patient",
+		list(
+			ID = "id"
+		),
+		list(
+			" ",
+			rm_empty_cols = FALSE
+		)
+	)
+)
+
+is_valid_design(design3)
+
+
+#fix df description with wrong order
+(f <- fix_df_desc(
+	df_desc = list(
+		style = list(
+			rm_empty_cols = FALSE,
+			brackets = c('[', ']'),
+			" >> "
+		),
+		"//Patient",
+		cols = list(
+			ID = "id"
+		)
+	)
+))
+
+#long design mit zwei Fehlern:
+designs <- list(
+	Pat1 = list(
+		resource = NULL
+	),
+	Pat2 = list(
+		resource = "//Patient",
+		cols = ".//*"
+	),
+	Pat3 = list(
+		resource = "//Patient",
+		cols  = list(
+			ID = "id"
+		)
+	),
+	Pat4 = list(
+		resource = 7,
+		cols = list(
+			ID = "id"
+		),
+		style = list(
+			sep = " ",
+			brackets = c("[", "]"),
+			rm_empty_cols = TRUE
+		)
+	),
+	Pat5 = list(
+		resource = "//Patient",
+		cols = ".//*",
+		style = list(
+			sep = " ",
+			brackets = c("[", "]"),
+			rm_empty_cols = TRUE
+		)
+	),
+	Pat6 = list(
+		resource = "//Patient",
+		cols = NULL, # wuerde man nicht machen
+		style = list(
+			sep = " ",
+			brackets = c("[", "]"),
+			rm_empty_cols = TRUE
+		)
+	),
+	Pat7 = list(
+		resource = "//Patient",
+		cols = NULL, # wuerde man nicht machen
+		style = list(
+			sep = " ",
+			brackets = "#"
+		)
+	),
+	Pat8 = list(
+		resource = "//Patient",
+		cols = NULL, # wuerde man nicht machen
+		list(
+			sep = " "
+		)
+	),
+	Pat9 = list(
+		"//Patient",
+		NULL,
+		list(
+			NULL,
+			"#"
+		)
+	),
+	Pat10 = list(
+		resource = "//Patient",
+		style = list(
+			rm_empty_cols = TRUE
+		)
+	),
+	Pat11 = list(
+		"//Patient"
+	),
+	Pat12 = list(
+		"//Patient",
+		".//*"
+	),
+	Pat13 = list(
+		"//Patient",
+		list(
+			ID = "id"
+		)
+	),
+	Pat14 = list(
+		"//Patient",
+		list(
+			ID = "id"
+		),
+		list(
+			" ",
+			c("[", "]"),
+			TRUE
+		)
+	),
+	Pat15 = list(
+		"//Patient",
+		".//*",
+		list(
+			" ",
+			c("[", "]"),
+			TRUE
+		)
+	),
+	Pat16 = list(
+		"//Patient",
+		NULL,
+		list(
+			" ",
+			c("[", "]"),
+			TRUE
+		)
+	),
+	Pat17 = list(
+		"//Patient",
+		NULL,
+		list(
+			" ",
+			"#"
+		)
+	),
+	Pat18 = list(
+		"//Patient",
+		NULL,
+		list(
+			" "
+		)
+	),
+	Pat19 = list(
+		"//Patient",
+		NULL,
+		list(
+
+		)
+	),
+	Pat20 = list(
+		"//Patient",
+		NULL,
+		list(
+			NULL,
+			NULL,
+			TRUE
+		)
+	)
+)
+
+is_valid_design(designs)
+
+
+
+assign("last.warning", NULL, envir = baseenv())
+(designs_fixed <- lapply(designs, fix))
+warnings()
+(is_valid_Design <- all(sapply(design, is_valid_design)))
+warnings()
+
+
+design2xml <- function (design) {
+
+	#dbg
+	#design <- designs
+	xml  <- xml2::xml_new_document()
+	root <- xml2::xml_add_child(xml, "Design")
+
+	for (nms in names(design)) {
+
+		#dbg
+		#nms <- names(design[2])
+		df_desc <- fix_df_desc(df_desc = design[[nms]])
+
+		if (is_valid_df_desc(df_desc = df_desc)[1,1]) {
+
+			child <- xml2::xml_add_child(root, nms)
+
+			res <- xml2::xml_add_child(child, "Resource")
+			xml2::xml_set_attr(res, "value", df_desc$resource)
+
+			cols <- xml2::xml_add_child(child, "Columns")
+
+			if (0 < length(df_desc$cols)) {
+
+				if (is.character(df_desc$cols)) {
+
+					xml2::xml_set_attr(cols, "value", df_desc$cols)
+				}
+				else {
+					for (nms_col in names(df_desc$cols)) {
+
+						col <- xml2::xml_add_child(cols, nms_col)
+						xml2::xml_set_attr(col, "value", df_desc$cols[[nms_col]])
+					}
+				}
+			}
+
+			stl <- xml2::xml_add_child(child, "Style")
+			sep <- xml2::xml_add_child(stl, "Sep")
+			bra <- xml2::xml_add_child(stl, "Brackets")
+			opn <- xml2::xml_add_child(bra, "Open")
+			cls <- xml2::xml_add_child(bra, "Close")
+			rme <- xml2::xml_add_child(stl, "Remove_Empty_Columns")
+
+			xml2::xml_set_attr(sep, "value", df_desc$style$sep)
+			xml2::xml_set_attr(opn, "value", df_desc$style$brackets[1])
+			xml2::xml_set_attr(cls, "value", df_desc$style$brackets[2])
+			xml2::xml_set_attr(rme, "value", df_desc$style$rm_empty_cols)
+		}
+	}
+	xml
+}
+
+save_design <- function (design, file = "design.xml") {
+
+	xml <- design2xml(design = design)
+
+	xml2::write_xml(xml, file)
+}
+
+save_design(design = designs, "Design.xml")
