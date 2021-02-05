450d9fa1d59777568bd2e8cffd007afb1f80be57
Julia Gantner <julia.gantner@med.uni-jena.de>
2021-02-05 21:17:08 +0100

    fix package reference, remove narrative elements, add entry/url

--- R/recover.R
+++ R/recover.R
@@ -1,29 +1,13 @@
-#
-# bun <- fhir_search("https://mii-agiop-3p.life.uni-leipzig.de/fhir/Patient",
-# 					  max_bundles = 1)
-#
-# b <- bun[[1]]
-#
-# design <- list(
-# 	Pat = list(
-# 		resource = "//Patient"
-# 	)
-# )
-#
-# dfs <- fhir_crack(bun, design)
-#
-# d <- dfs[[1]]
-#
-#
-# library(xml2)
+
+
 
 #TODO: remove div elements, deal with multiple entries
 
 #create empty resource
 create_resource <- function(resourceType, colnames){
 
-	myres <- read_xml("<resource> </resource>")
-	xml_add_child(myres, xml_new_root(resourceType))
+	myres <- xml2::read_xml("<resource> </resource>")
+	xml2::xml_add_child(myres, xml2::xml_new_root(resourceType))
 
 
 	nodes <- strsplit(colnames, esc("."))
@@ -43,8 +27,8 @@ create_resource <- function(resourceType, colnames){
 
 
 		for (i in 1:length(current_red)){
-			parent_node <- xml_child(myres, parent_red[i])
-			xml_add_child(parent_node, xml_new_root(current_red[i]))
+			parent_node <- xml2::xml_child(myres, parent_red[i])
+			xml2::xml_add_child(parent_node, xml2::xml_new_root(current_red[i]))
 		}
 
 		parent <- paste(parent,current, sep="/")
@@ -59,38 +43,49 @@ create_resource <- function(resourceType, colnames){
 #fill
 fill_resource <- function(resource, row){
 
-	resourceType <- xml_name(xml_child(resource, 1))
+	resourceType <- xml2::xml_name(xml2::xml_child(resource, 1))
 
 	paths <- gsub(esc("."), esc("/"),names(row))
 	paths <- paste(resourceType, paths, sep="/")
 
 	for(i in 1:ncol(row)){
-		child <- xml_child(resource, paths[i])
-		xml_attr(child, "value")  <- row[1,i]
+		child <- xml2::xml_child(resource, paths[i])
+		xml2::xml_attr(child, "value")  <- row[1,i]
 	}
 }
 
-res <- create_resource("Patient", names(d))
-fill_resource(res, d[2,])
 
 #
 create_bundle <- function(resourceType, df){
-	#l <- vector("list", nrow(df))
 
-	bundle <- xml_new_root("Bundle")
-	xml_add_child(bundle, xml_new_root("type", value= "transaction"))
+	#remove any narrative elements https://www.hl7.org/fhir/narrative.html
+	if(any(grepl("^text", names(df)))){
+		d <- df[,!names(df) %in% fhir_common_columns(df, "text")]
+	}else{
+		d <- df
+	}
 
-	for(i in 1:nrow(df)){
-		xml_add_child(bundle, create_resource(resourceType = resourceType, colnames = names(df)))
-		fill_resource(xml_child(bundle, i+1), row = df[i,])
-		xml_add_parent(xml_child(bundle, i+1), xml_new_root("entry"))
-		xml_add_child(xml_child(bundle, i+1), xml_new_root("request"))
-		xml_add_child(xml_find_all(bundle, "entry/request")[[i]], xml_new_root("method", value="PUT"))
-		xml_find_all(bundle, "entry")
 
+	#create transaction bundle
+	bundle <- xml2::xml_new_root("Bundle")
+	xml2::xml_add_child(bundle, xml2::xml_new_root("type", value= "transaction"))
+
+	for(i in 1:nrow(d)){
+		#create and fill resources
+		xml2::xml_add_child(bundle, create_resource(resourceType = resourceType, colnames = names(d)))
+		fill_resource(xml2::xml_child(bundle, i+1), row = d[i,])
+		#create entry and request nodes
+		xml2::xml_add_parent(xml2::xml_child(bundle, i+1), xml2::xml_new_root("entry"))
+		xml2::xml_add_child(xml2::xml_child(bundle, i+1), xml2::xml_new_root("request"))
+		xml2::xml_add_child(xml2::xml_find_all(bundle, "entry/request")[[i]], xml2::xml_new_root("method", value="PUT"))
+		xml2::xml_add_child(xml2::xml_find_all(bundle, "entry/request")[[i]],
+							xml2::xml_new_root("url",
+											   value=paste(resourceType,
+											   			 xml2::xml_attr(xml2::xml_child(bundle, "entry/resource/*/id"), "value"),
+											   			sep="/")))
 	}
 
 	bundle
 }
 
-#erg <- create_bundle("Patient", d)
+
