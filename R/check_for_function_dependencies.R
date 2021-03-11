9f9d5d7df477f9442bc521468d4639c9d037395e
Thomas Peschel <tpeschel@imise.uni-leipzig.de>
2021-03-12 00:39:30 +0100

    Fix some errors

--- R/check_for_function_dependencies.R
+++ R/check_for_function_dependencies.R
@@ -5,23 +5,15 @@ name <- function(v){
 	v
 }
 
-all_scripts <- list.files("R", ".R")
+all_scripts <- setdiff(list.files("R", ".R"), "check_for_function_dependencies.R")
 
 all_txt <- lapply(
 	name(all_scripts),
 	function(as) {
-		readLines(paste0("R/", as))
-	}
-)
-
-
-all_txt <- lapply(
-	all_txt,
-	function(txt_) {
+		txt_ <- readLines(paste0("R/", as))
 		txt_[txt_ != "" & !grepl("^#", txt_)]
 	}
 )
-
 
 all_funs <- lapply(
 	all_txt,
@@ -34,40 +26,53 @@ really_all_funs <- sort(unique(unlist(all_funs)))
 
 dep <- list(Nomfun = data.table::data.table(label = really_all_funs, id = seq_along(really_all_funs)))
 
-dep$fromto <-data.table::rbindlist(
+dep$fromto <- data.table::rbindlist(
 	lapply(
 		all_txt,
 		function(txt_) {
-			#txt_ <- all_txt[[5]]
 			data.table::rbindlist(
 				lapply(
 					really_all_funs,
 					function(raf) {
-						#raf <- really_all_funs[[9]]
 						from <- grep(paste0("^", raf, " *<- *function *\\("), txt_)
 						if(0 < length(from)) {
 							if(1 < length(from)) print(raf)
 							to <- grep("^}$", txt_)
 							to <- to[min(which(0 < to - from))]
 							id <- dep$Nomfun$id[dep$Nomfun$label == raf]
-							emb_funs <- sapply(really_all_funs, grep, txt_[from:to])
+							emb_funs <- sapply(
+								really_all_funs,
+								function(raf_) {
+									grep(paste0(raf_, " *\\("), txt_[from : to])
+								}
+							)
 							emb_funs <- names(emb_funs[sapply(emb_funs, function(ef) 0 < length(ef))])
-							data.table::data.table(from = id, to = match(emb_funs, really_all_funs))
+							if(0 < length(emb_funs))
+								data.table::data.table(from = id, to = match(emb_funs, really_all_funs))
 						}
 					}
 				)
 			)
-			# all_all <- all_all[!sapply(all_all, is.null)]
-			# a
 		}
 	)
 )
 
-dep$fromto <- unique(dep$fromto[dep$fromto$from!=dep$fromto$to,])
+dep$fromto <- unique(dep$fromto[from != to,])
 
-attributes(dep)
 attr(dep, "class") <- "dependenciesGraphs"
-dep
-dep_
+
 plot(dep)
 
+# base functions,
+# don't use any functions itself
+setdiff(
+	sort(dep$Nomfun$label[unique(dep$fromto$to)]),
+	sort(dep$Nomfun$label[unique(dep$fromto$from)])
+)
+
+# end functions,
+# are not used by any other functions
+setdiff(
+	sort(dep$Nomfun$label[unique(dep$fromto$from)]),
+	sort(dep$Nomfun$label[unique(dep$fromto$to)])
+)
