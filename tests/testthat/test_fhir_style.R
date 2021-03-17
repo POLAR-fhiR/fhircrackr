b416205e626884d54968c6d652507e96fb8c8b63
Julia Palm <julia.gantner@med.uni-jena.de>
2021-03-17 12:30:44 +0100

    tests for fhir_style class

---
+++ tests/testthat/test_fhir_style.R
@@ -0,0 +1,19 @@
+
+testthat::test_that(
+	"fhir_style is constructed correctly", {
+
+		style <- fhir_style(sep = "|", brackets = c("[", "]"), rm_empty_cols = T)
+
+		testthat::expect_s4_class(style, "fhir_style")
+	}
+)
+
+
+testthat::test_that(
+	"fhir_style throws error for invalid input", {
+
+		testthat::expect_error(fhir_style(sep = 3))
+		testthat::expect_error(fhir_style(brackets = c("[")))
+		testthat::expect_error(fhir_style(rm_empty_cols = 2))
+	}
+)
