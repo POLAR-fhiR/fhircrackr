b0c0c796704a4680e6d922cf9012823c9046e4cd
Julia Palm <julia.gantner@med.uni-jena.de>
2021-03-22 16:32:35 +0100

    change constructor

--- R/fhir_parameters.R
+++ R/fhir_parameters.R
@@ -7,38 +7,45 @@
 #' @slot params A list of fhir_key_value_pair objects
 #'
 
-setClass("fhir_parameters",
-		 slots = c(param_pairs="list", param_string = "character"))
+setClass(
+	"fhir_parameters",
+	slots = c(param_pairs="list", param_string = "character")
+)
 
 #validity
-setValidity("fhir_parameters",
-			function(object){
-			messages <- c()
+setValidity(
+	"fhir_parameters",
+	function(object){
+		messages <- c()
 
-			if(any(!sapply(object@param_pairs, function(x){class(x)=="fhir_key_value_pair"}))){
-				messages <- c(messages, "fhir_parameters can only contain fhir_key_value_pair objects.")
-			}
+		if(any(!sapply(object@param_pairs, function(x){class(x)=="fhir_key_value_pair"}))){
+			messages <- c(messages, "fhir_parameters can only contain fhir_key_value_pair objects.")
+		}
 
-			if(length(object@param_string)>1){
-				messages <- c(messages, "param_string must be of length one")
-			}
+		if(length(object@param_string)>1){
+			messages <- c(messages, "param_string must be of length one")
+		}
 
-			if(length(messages)>0){messages}else{TRUE}
-			})
+		if(length(messages)>0){messages}else{TRUE}
+	}
+)
 
 
 #Initialize function
 #only for internal use, creates param_string
-setMethod("initialize", "fhir_parameters",
-		  function(.Object,...){
-		  	.Object <- callNextMethod()
+setMethod(
+	"initialize",
+	"fhir_parameters",
+	function(.Object,...){
+	  	.Object <- callNextMethod()
 
-		  	#paste
-		  	pairs <- lapply(.Object@param_pairs, function(x){paste(x@key, x@value, sep="=")})
-		  	.Object@param_string <- paste(pairs, collapse = "&")
+	  	#paste
+	  	pairs <- lapply(.Object@param_pairs, function(x){paste(x@key, x@value, sep="=")})
+	  	.Object@param_string <- paste(pairs, collapse = "&")
 
-		  	.Object
-		  })
+	  	.Object
+	 }
+)
 
 
 #constructor for user
@@ -46,70 +53,118 @@ setMethod("initialize", "fhir_parameters",
 
 #' Create [fhir_parameters-class] object
 #'
-#' A [fhir_parameters-class] object can be created in two different ways: Either you provide
-#' a length 1 character with all the search parameters pasted together properly.
-#' Or you provide a list with length two character vectors containing key value pairs.See examples.
+#' A [fhir_parameters-class] object can be created in three ways:
+#'  - You provide a length 1 character with all the search parameters pasted together properly
+#'  - You provide a list with length two character vectors containing key value pairs.
+#'  - You pass a number of fhir_key_value_pair objects to the function.
+#'  See examples.
 #'
-#' @param params Either a length 1 character containing properly formatted FHIR search parameters, e.g.
+#' @param ... Either a length 1 character containing properly formatted FHIR search parameters, e.g.
 #' `"gender=male&_summary=count"` or list of length 2 character vectors each representing one key value pair,
 #' with the first element as the key and the second element as the value, e.g.
-#' `list(c("gender", "male"), c("_summary", "count"))`
+#' `list(c("gender", "male"), c("_summary", "count"))`or one or multiple fhir_key_value_pair objects
 #'
 #' @examples
-#' #Two ways to create the same fhir_parameters object
+#' #Three ways to create the same fhir_parameters object
 #'
 #' #using a string
-#' fhir_parameters(params = "gender=male&birthdate=le2000-01-01&_summary=count")
+#' fhir_parameters("gender=male&birthdate=le2000-01-01&_summary=count")
 #'
 #' #using a list
-#' fhir_parameters(params = list(c("gender", "male"),
-#'                                   c("birthdate", "le2000-01-01"),
-#'                                   c("_summary", "count")))
-#'
+#' fhir_parameters(list(c("gender", "male"),
+#'                      c("birthdate", "le2000-01-01"),
+#'                      c("_summary", "count")
+#'                  )
+#'  )
+#' # using key value pairs
+#' fhir_parameters(new("fhir_key_value_pair", key="gender", value="male"),
+#'                 new("fhir_key_value_pair", key="birthdate", value="le2000-01-01"),
+#'                 new("fhir_key_value_pair", key="_summary", value="count"))
 
-setGeneric("fhir_parameters", function(params){
-	standardGeneric("fhir_parameters")
-})
+setGeneric(
+	"fhir_parameters",
+	function(...){
+		standardGeneric("fhir_parameters")
+	}
+)
 
-setMethod("fhir_parameters", signature = c(params= "character"),
-		  function(params){
-		  	if(length(params)>1){
-		  		stop("When using a character, argument params has to be of length one.")
-		  	}
+setMethod(
+	"fhir_parameters",
+	signature = c(...="fhir_key_value_pair"),
+	function(...){
+		l <- list(...)
+		classes <- sapply(l, class)
 
-		  	pairs <- strsplit(params, "&", fixed=T)[[1]]
-		  	pairs <- strsplit(pairs, "=")
+		if(length(unique(classes))>1){stop("You cannot mix input classes")}
 
-		  	list <- lapply(pairs, function(x){new("fhir_key_value_pair", key= x[1], value=x[2])})
+		new("fhir_parameters", param_pairs = l)
+	}
+)
 
-		  	new("fhir_parameters", param_pairs=list)
-		  })
+setMethod(
+	"fhir_parameters",
+	signature = c(...= "character"),
+	function(...){
+		l <- list(...)
+		classes <- sapply(l, class)
 
-setMethod("fhir_parameters", signature = c(params= "list"),
-		  function(params){
+		if(length(unique(classes))>1){stop("You cannot mix input classes")}
 
-		  	if(any(!sapply(params, is.character))){
-		  		stop("All list elements for argument params must be of type character")
-		  	}
+		if(length(l)>1){
+	  		stop("You can only provide one character to this function.")
+	  	}
+		l <- unlist(l)
+		if(length(l)>1){
+			stop("If you provide a character to this function, it has to be of length 1.")
+		}
+	  	pairs <- strsplit(l, "&", fixed=T)[[1]]
+	  	pairs <- strsplit(pairs, "=")
 
-		  	if(any(sapply(params, length)!=2)){
-		  		stop("All list elements for argument params must have exactly length two")
-		  	}
+	  	list <- lapply(pairs, function(x){new("fhir_key_value_pair", key= x[1], value=x[2])})
 
-		  	list <- lapply(params, function(x){new("fhir_key_value_pair", key= x[1], value=x[2])})
+	  	new("fhir_parameters", param_pairs=list)
+	}
+)
 
-		  	new("fhir_parameters", param_pairs = list)
-		  })
+setMethod(
+	"fhir_parameters",
+	signature = c(...="list"),
+	function(...){
+		l <- list(...)
+		classes <- sapply(l, class)
 
+		if(length(unique(classes))>1){stop("You cannot mix input classes")}
 
+		if(length(l)>1){
+			stop("You can only provide one list to this function.")
+		}
+
+		l <- unlist(l, recursive = F)
+
+	  	if(any(!sapply(l, is.character))){
+	  		stop("All list elements of the provided list must be of type character")
+	  	}
+
+	  	if(any(sapply(l, length)!=2)){
+	  		stop("All list elements must be exactly of length two")
+	  	}
+
+	  	list <- lapply(l, function(x){new("fhir_key_value_pair", key= x[1], value=x[2])})
+
+	  	new("fhir_parameters", param_pairs = list)
+	}
+)
 
 #show method
-setMethod("show", "fhir_parameters",
-		  function(object){
-		  	cat(paste0(
-		  		"URL-encoded parameter string for FHIR search:\n", object@param_string,"\n\n"		  		)
-		  	)
-		  })
+setMethod(
+	"show",
+	"fhir_parameters",
+	function(object){
+		cat(paste0(
+			"URL-encoded parameter string for FHIR search:\n", object@param_string,"\n\n"		  		)
+		)
+	}
+)
 
 
 
--- tests/testthat/test_fhir_parameters.R
+++ tests/testthat/test_fhir_parameters.R
@@ -2,20 +2,31 @@
 testthat::test_that(
 	"methods for fhir_parameters() create identical results", {
 
-		p1 <- fhir_parameters(params = "gender=male&birthdate=le2000-01-01&_summary=count")
-		p2 <- fhir_parameters(params = list(c("gender", "male"),
-											   c("birthdate", "le2000-01-01"),
-											   c("_summary", "count")))
+		p1 <- fhir_parameters("gender=male&birthdate=le2000-01-01&_summary=count")
+
+		p2 <- fhir_parameters(
+			list(
+				c("gender", "male"),
+				c("birthdate", "le2000-01-01"),
+				c("_summary", "count")
+				)
+			)
+
+		p3 <- fhir_parameters(
+			new("fhir_key_value_pair", key="gender", value="male"),
+		    new("fhir_key_value_pair", key="birthdate", value="le2000-01-01"),
+		    new("fhir_key_value_pair", key="_summary", value="count"))
 
 		testthat::expect_identical(p1,p2)
+		testthat::expect_identical(p3,p2)
 	}
 )
 
 testthat::test_that(
 	"errors are thrown for incorrect input", {
 
-		testthat::expect_error(fhir_parameters(params = list(c("a", "b"), c("a"))))
-		testthat::expect_error(fhir_parameters(params = list(c(1, 2))))
+		testthat::expect_error(fhir_parameters(list(c("a", "b"), c("a"))))
+		testthat::expect_error(fhir_parameters(list(c(1, 2))))
 
 	}
 )
