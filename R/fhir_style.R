f69c5d32312fb424bf52c9432d1909ff23d337c5
Thomas Peschel <tpeschel@imise.uni-leipzig.de>
2021-03-22 13:55:51 +0100

    Minor Changes in fhir_style

--- R/fhir_style.R
+++ R/fhir_style.R
@@ -7,28 +7,25 @@
 #' e.g. `c( "<", ">")`. If this is empty (i.e. character of length 0), no indices will be added to multiple entries.
 #' @slot rm_empty_cols Logical scalar. Remove empty columns?
 #'
-setClass("fhir_style",
-		 slots = c(sep = "character",
-		 		  brackets = "character",
-		 		  rm_empty_cols = "logical"))
+setClass(
+	"fhir_style",
+	slots = c(
+		sep = "character",
+		brackets = "character",
+		rm_empty_cols = "logical"
+	)
+)
 
 #Validity check
-setValidity("fhir_style",
-			method = function(object){
-				messages <- c()
-
-				if(length(object@sep) > 1){
-					messages <- c(messages, "sep must be character of length 1")
-				}
-				if(!length(object@brackets) %in% c(0,2)){
-					messages <- c(messages, "brackets must be character of length 2 or empty")
-				}
-				if(length(object@rm_empty_cols) > 1){
-					messages <- c(messages, "remove_empty_columns must be logical of length 1")
-				}
-
-				if(length(messages)>0){messages}else{TRUE}
-			}
+setValidity(
+	"fhir_style",
+	method = function(object) {
+		messages <- c()
+		if(1 < length(object@sep)) messages <- c(messages, "sep must be character of length 1")
+		if(!length(object@brackets) %in% c(0, 2)) messages <- c(messages, "brackets must be character of length 2 or empty")
+		if(1 < length(object@rm_empty_cols)) messages <- c(messages, "remove_empty_columns must be logical of length 1")
+		if(0 < length(messages)) messages else TRUE
+	}
 )
 
 #Constructor
@@ -46,24 +43,18 @@ setValidity("fhir_style",
 #' fhir_style(sep = " ", brackets = c("[", "]"), rm_empty_cols = FALSE)
 
 
-fhir_style <- function(sep=" ", brackets=character(), rm_empty_cols=TRUE){
- new("fhir_style", sep=sep, brackets=brackets, rm_empty_cols=rm_empty_cols)
+fhir_style <- function(sep=" ", brackets=character(), rm_empty_cols=TRUE) {
+	new("fhir_style", sep=sep, brackets=brackets, rm_empty_cols=rm_empty_cols)
 }
 
 
 #methods
-setMethod("show", signature = "fhir_style",
-		  function(object){
-		  	sep <- ifelse(object@sep==" ", paste0("'", object@sep, "'"), object@sep)
-
-		  	brackets <- ifelse(length(object@brackets)==0,
-		  					   "character(0)",
-		  					   paste0("'", object@brackets[1], "' '", object@brackets[2], "'"))
-
-		  	cat(paste0(
-		  		"fhir_style object with the following elements:\n\n",
-		  		"sep: ", sep, "\n",
-		  		"brackets: ", brackets, "\n",
-		  		"rm_empty_cols: ", object@rm_empty_cols))
-
-		  })
+setMethod(
+	"show",
+	signature = "fhir_style",
+	function(object) {
+		sep <- if(object@sep==" ") paste0("'", object@sep, "'") else object@sep
+		brackets <- if(length(object@brackets)==0) "character(0)" else paste0("'", object@brackets[1], "' '", object@brackets[2], "'")
+		cat(paste0("fhir_style object with the following elements:\n\nsep: ", sep, "\nbrackets: ", brackets, "\nrm_empty_cols: ", object@rm_empty_cols))
+	}
+)
