593838a8055daf1030c584d78e2c1ad7357d3472
Julia Palm <julia.gantner@med.uni-jena.de>
2021-03-23 08:21:00 +0100

    format code

--- R/fhir_url.R
+++ R/fhir_url.R
@@ -1,20 +1,20 @@
 #Class definition
-setClass("fhir_url",
-		 contains = "VIRTUAL",
-		 slots=c(base = "character"))
+setClass(
+	"fhir_url",
+	contains = "VIRTUAL",
+	slots=c(base = "character")
+)
 
 #Validity check
-setValidity("fhir_url",
-			method = function(object){
-				messages <- c()
-
-				if(length(object@base) > 1 ){
-					messages <- c(messages, paste0("The base for a fhir_url has to be a character of length 1."))
-				}
-
-				if(length(messages)>0){messages}else{TRUE}
-
-			}
+setValidity(
+	"fhir_url",
+	method = function(object){
+		messages <- c()
+		if(length(object@base) > 1 ){
+			messages <- c(messages, paste0("The base for a fhir_url has to be a character of length 1."))
+		}
+		if(length(messages)>0){messages}else{TRUE}
+	}
 )
 
 #no constructor for virtual class
