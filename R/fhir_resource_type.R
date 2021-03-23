c0d92e6cdfc6d815cef7980b615464e57b61e7c3
Julia Palm <julia.gantner@med.uni-jena.de>
2021-03-23 08:33:53 +0100

    format code, document

--- R/fhir_resource_type.R
+++ R/fhir_resource_type.R
@@ -1,24 +1,27 @@
 #Class definition
-setClass("fhir_resource_type",
-		 contains = "character")
 
-#Validity check
-setValidity("fhir_resource_type",
-			method = function(object){
-				messages <- c()
-
-				if(length(object) > 1 ){
-					messages <- c(messages, paste0("Please provide only a single string to define the FHIR resource."))
-				}
-
-				if(length(messages)>0){messages}else{TRUE}
-
-			}
+#' A representation of a FHIR resource type
+setClass(
+	"fhir_resource_type",
+	contains = "character"
 )
 
-#' Create fhir_resource object
+#Validity check
+setValidity(
+	"fhir_resource_type",
+	method = function(object){
+		messages <- c()
+
+		if(length(object) > 1 ){
+			messages <- c(messages, paste0("Please provide only a single string to define the FHIR resource."))
+		}
+		if(length(messages)>0){messages}else{TRUE}
+	}
+)
+
+#' Create [fhir_resource_type-class] object
 #'
-#' This function creates an object of class `fhir_resource_type` by taking a string defining a FHIR resource type
+#' This function creates an object of class [fhir_resource_type_class] by taking a string defining a FHIR resource type
 #' and formating it correctly,i.e. removing white space and slashes. It also checks the resource against the list
 #' of resources provided at https://hl7.org/FHIR/resourcelist.html and converts cases appropriately
 #' if the resource is found in that list.
@@ -45,4 +48,4 @@ fhir_resource_type <- function(string){
 	#second time: run with corrected input
 	new("fhir_resource_type", string)
 }
-fhir_resource_type("patient")
+
