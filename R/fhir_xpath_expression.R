9d8be098ae5c93531e551f99cac07afdb634dff7
Julia Palm <julia.gantner@med.uni-jena.de>
2021-03-23 08:28:48 +0100

    format code

--- R/fhir_xpath_expression.R
+++ R/fhir_xpath_expression.R
@@ -1,34 +1,36 @@
 
-#' A
+#' A fhir_xpath_expression object
 
 #definition
-setClass("fhir_xpath_expression",
-		 contains = "character")
+setClass(
+	"fhir_xpath_expression",
+	contains = "character"
+)
 
 #validity check
-setValidity("fhir_xpath_expression",
-			method = function(object){
-				messages <- c()
+setValidity(
+	"fhir_xpath_expression",
+	method = function(object){
+		messages <- c()
+		if(length(object)>1){
+			messages <- c(messages, "A fhir_xpath_expression hs to be of lenght 1.")
+		}
 
-				if(length(object)>1){
-					messages <- c(messages, "A fhir_xpath_expression hs to be of lenght 1.")
-				}
+		#slightly hacky solution: use xml2 function and catch warning message
+		#this will validate xpath expression with libxml2 (accessed by xml2)
+		testbundle <- xml2::read_xml("<Bundle>   </Bundle>")
 
-				#slightly hacky solution: use xml2 function and catch warning message
-				#this will validate xpath expression with libxml2 (accessed by xml2)
-				testbundle <- xml2::read_xml("<Bundle>   </Bundle>")
-
-				tryCatch(
-					xml2::xml_find_all(testbundle, object),
-					warning = function(x) {
-						if (grepl("Invalid expression", x))
-							messages <<- c(messages, paste(esc(object),
-								"is not a valid XPath expression."))
+		tryCatch(
+			xml2::xml_find_all(testbundle, object),
+			warning = function(x) {
+				if (grepl("Invalid expression", x))
+					messages <<- c(messages, paste(esc(object),"is not a valid XPath expression.")
+								   )
 					}
 				)
-
-				if(length(messages)>0){messages}else{TRUE}
-			})
+		if(length(messages)>0){messages}else{TRUE}
+	}
+)
 
 #' Create fhir_xpath_expression
 #'
