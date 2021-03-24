aa6a1ebc3fa64cf6f73164b34510585dfb510fd9
Thomas Peschel <tpeschel@imise.uni-leipzig.de>
2021-03-24 11:01:00 +0100

    Final Minor Changes

--- R/fhir_resource_type.R
+++ R/fhir_resource_type.R
@@ -33,13 +33,10 @@ setValidity(
 fhir_resource_type <- function(string) {
 
 	result <- new("fhir_resource_type", string)
-
 	if(!string %in% existing_resource_types) {
 		warning("The string you provided doesn't match any of the resource types defined under https://hl7.org/FHIR/resourcelist.html. ",
 				"Case matters! If you are sure the resource type is correct anyway, you can ignore this warning.")
 	}
-
 	result
-
 }
 
--- tests/testthat/test_fhir_resource_type.R
+++ tests/testthat/test_fhir_resource_type.R
@@ -2,10 +2,8 @@
 testthat::test_that(
 	"fhir_resource_type creates object correctly", {
 
-		pat <- fhir_resource_type("Patient")
-
-		testthat::expect_s4_class(object = pat, class = "fhir_resource_type")
-		testthat::expect_true(pat %in% existing_resource_types)
+		testthat::expect_s4_class(object = fhir_resource_type("Patient"), class = "fhir_resource_type")
+		testthat::expect_true(fhir_resource_type("Patient") %in% existing_resource_types)
 	}
 )
 
