1ed58c217bf5525fc2cc2f94b4d400e197b1ab2f
Thomas Peschel <tpeschel@imise.uni-leipzig.de>
2021-09-09 15:36:10 +0200

    Collect Sample Resources via post

--- R/sample_resources.R
+++ R/sample_resources.R
@@ -1,127 +1,209 @@
 MAX_LEN = 2083
 
-get_resources_count <- function(endpoint, resource_name) {
-	as.numeric(xml2::xml_attr(xml2::xml_find_first(fhir_search(paste0(paste_paths(endpoint, resource_name), "?_summary=count"), verbose = 0, log_errors = "tmp/log_resources_counts.txt")[[1]], "//total"), "value"))
+dyadic <- function(x, y, fun = function(x, y) x * y, ...) {
+	d <- data.table()
+	n <- as.character(y)
+	d[,(n):=lapply(y, function(y_) lapply(x, function(x_) fun(x_, y_, ...)))]
+	d$row <- as.character(x)
+	setcolorder(d, c("row", n))
+	d[]
 }
 
-get_resources_ids <- function(endpoint = endpoints$vonk, resource_name) {
-	cat(paste0("Download ", get_resources_count(endpoint = endpoint, resource_name = resource_name), " ", resource_name, "s' IDs...\n"))
-	unlist(
+order_correctly_strings_with_numbers <- function(s) {
+	sc <- as.character(s)
+	ss <- stringr::str_extract_all(sc, "[0-9]+")
+	i<-1
+	max_char <- max(sapply(ss, function(x) {if(0 < length(x) && !is.na(x)) max(nchar(x), na.rm = T) else 0}))
+	(s_numbers <- strsplit(sc, "[^0-9]+"))
+	(s_letters <- strsplit(sc, "[0-9]+"))
+
+	order(
 		sapply(
-			fhir_search(paste0(paste_paths(endpoint, resource_name), "?_count=500&_elements=id"), log_errors = "tmp/log_get_resources_ids.txt", verbose = 2),
-			function(bundle) {
-				xml2::xml_attr(xml2::xml_find_all(bundle, paste0("entry/resource/", resource_name, "/id")), "value")
+			seq_along(ss),
+			function(i) {
+				sn <- s_numbers[[i]]
+				sl <- s_letters[[i]]
+				if(length(sn) < length(sl)) {
+					sn <- c(sn, "")
+				} else if(length(sl) < length(sn)) {
+					sl <- c(sl, "")
+				}
+				sn <- ifelse(!is.na(sn) && sn != "", stringr::str_pad(sn, max_char, "left", "0"), "")
+				if(sn[1] != "") paste0(sl, sn, collapse = "") else paste0(sn, sl, collapse = "")
 			}
 		)
 	)
 }
 
-collect_indices_for_request <- function(ids, max_ids = length(ids), max_len = MAX_LEN - 300) {
-	n <- 1
-	s <- ids[[n]]
-	n <- n + 1
-	while(n <= max_ids && n <= length(ids) && nchar(s) + nchar(ids[[n]]) < max_len) {
-		s <- paste0(s, ",", ids[[n]])
-		n <- n + 1
+sort_correctly_strings_with_numbers <- function(s) {
+	if(is.null(s)){
+
+		return(s)
 	}
-	list(str = s, n = n - 1)
+	sc <- as.character(s)
+	s[order_correctly_strings_with_numbers(s)]
 }
 
-# ids <- get_resources_ids(endpoint = endpoints$agiop, resource_name = resource_name)
-# nchar(collect_indices_for_request(ids = ids, max_len = 20)$str)
+add_parameter <- function(parameters, parm2add = c("summary" = "count")) {
+	if(is.null(parameters) || nchar(parameters) < 1) {
+		parm2add
+	} else if(length(names(parameters)) < length(parameters)) {
+		paste0(
+			parameters,
+			if(0 < nchar(parameters)) "&" else "?",
+			names(parm2add), "=", parm2add
+		)
+	} else {
+		c(parameters, parm2add)
+	}
+}
 
+get_resources_count <- function(endpoint, resource_name, parameters = NULL, verbose = 0) {
+	as.numeric(
+		xml2::xml_attr(
+			xml2::xml_find_first(
+				fhir_search(
+					fhir_url(
+						url = endpoint,
+						resource = resource_name,
+						parameters = add_parameter(parameters, c("_summary" = "count")),
+					),
+					verbose = verbose
+				)[[1]],
+				"//total"
+			),
+			"value"
+		)
+	)
+}
 
-# get_samples <- function(endpoint, resource_name, sample_size = 50, bundle_size = 7, seed = as.double(Sys.time())) {
-# 	set.seed(seed = seed)
-# 	cnt <- get_resources_count(endpoint = endpoint, resource_name = resource_name)
-# 	if(cnt < sample_size) stop("The sample must be smaller or equal than the population size.")
-# 	ids <- get_resources_ids(endpoint = endpoint, resource_name = resource_name)
-# 	ids <- try(sort(sample(ids, sample_size, replace = F)))
-# 	if(inherits(ids, "try-error"))  stop("The sample must be smaller or equal than the population size.")
+get_resources_ids <- function(endpoint = endpoints$vonk, resource_name, parameters = NULL, verbose = 0) {
+	if(0 < verbose) {
+		cat(
+			paste0(
+				"Download ",
+				get_resources_count(
+					endpoint = endpoint,
+					resource_name = resource_name,
+					parameters = parameters
+				),
+				" ",
+				resource_name,
+				"s' IDs...\n"
+			)
+		)
+	}
+	bundles <- try(
+		fhir_search(
+			url <- fhir_url(
+				url = endpoint,
+				resource = resource_name,
+				parameters = parameters %>%
+					add_parameter(c("_elements" = "id")) %>%
+					add_parameter(c("_count" = "500"))
+			),
+			verbose = verbose
+		)
+	)
+
+	if(inherits(bundles, "try-error")) {
+		warning(paste0("The url ", url, " could not be succesfully resolved. Use fhir_recent_http_error() to get more information!"))
+		NA_integer_;
+	} else {
+		unlist(
+			lapply(
+				bundles,
+				function(bundle) {
+					xml2::xml_attr(
+						xml2::xml_find_all(
+							bundle,
+							paste0(
+								"entry/resource/",
+								resource_name,
+								"/id"
+							)
+						),
+						"value"
+					)
+				}
+			)
+		)
+	}
+}
+
+collect_indices_for_request <- function(ids, max_ids = length(ids), max_len = MAX_LEN - 300) {
+	if(length(ids) < 1) {
+		warning(
+			paste0(
+				"The length of ids is zero. So no single id is added to the list."
+			)
+		)
+		list(str = "", n = 0)
+	} else if(max_len < nchar(ids[[1]])) {
+		warning(
+			paste0(
+				"The length of the first id string ",
+				ids[[1]],
+				" is already greater than max_len=",
+				max_len,
+				". So no single id is added to the list."
+			)
+		)
+		list(str = "", n = 0)
+	} else {
+		n <- 1
+		s <- ids[[n]]
+		while(n < max_ids && n < length(ids) && nchar(s) + nchar(ids[[n + 1]]) < max_len) {
+			n <- n + 1
+			s <- paste0(s, ",", ids[[n]])
+		}
+		list(str = s, n = n)
+	}
+}
+
+# get_resources_by_ids_fix_bundle_size <- function(endpoint, resource_name, ids, bundle_size = 7, seed = as.double(Sys.time())) {
+# 	# cnt <- get_resources_count(endpoint = endpoint, resource_name = resource_name)
+# 	# if(cnt < sample_size) stop("The sample must be smaller or equal than the population size.")
+# 	# ids <- get_resources_ids(endpoint = endpoint, resource_name = resource_name)
+# 	# set.seed(seed = seed)
+# 	# ids <- try(sample(ids, sample_size, replace = F))
+# 	# #	ids <- sort(ids)
+# 	# if(inherits(ids, "try-error"))  stop("The sample must be smaller or equal than the population size.")
 #
 # 	bundles <- list()
-# 	i <- 1
-# 	b <- 1
-# 	while(i <= sample_size) {
-# 		end <- min(c(i + bundle_size - 1, sample_size, length(ids)))
+# 	total <- 1
+# 	bundle_count <- 1
+# 	while(0 < length(ids)) {
 # 		blist <- xml2::xml_new_root("Bundle")
-# 		j <- 1
-# 		while(i <= end) {
-# 			ids_ <- collect_indices_for_request(ids = ids, 1200)
+# 		end <- min(c(bundle_size, length(ids)))
+# 		current_bundle_size <- 0
+# 		while(current_bundle_size < end) {
+# 			ids_ <- collect_indices_for_request(ids = ids, max_ids = end - current_bundle_size, 1200)
 # 			url_ <- paste0(paste_paths(endpoint, resource_name), "?_id=", ids_$str)
-# #			url_ <- paste0(paste_paths(endpoint, resource_name), "?_id=", paste0(ids[i : (i + 10)], collapse = ","))
-# 			bnd_ <- fhir_search(request = url_, max_bundles = 1, verbose = 0)
-# 			entries <- xml2::xml_find_all(bnd_[[1]], paste0("entry[./resource/", resource_name,"]"))
-# 			for(entry in entries) xml2::xml_add_child(blist, entry)
-# 			# entry <- xml2::xml_find_first(bnd_[[1]], paste0("entry[./resource/", resource_name,"]"))
-# 			# xml2::xml_add_child(blist, entry)
-# 			i <- i + ids_$n
-# 			j <- j + ids_$n
+# 			bnd_ <- fhir_search(request = url_, verbose = 0, log_errors = "tmp/log_get_resources_fix_bundlesize.txt")
+# 			for(bn in bnd_) {
+# 				entries <- xml2::xml_find_all(bn, paste0("entry[./resource/", resource_name,"]"))
+# 				for(entry in entries) xml2::xml_add_child(blist, entry)
+# 			}
+# 			total <- total + ids_$n
+# 			current_bundle_size <- current_bundle_size + ids_$n
 # 			ids <- ids[-seq_len(ids_$n)]
 # 		}
-# 		bundles[[b]] <- blist
-# 		cat(paste0("Bundle ", b, " a ", j - 1, " ", resource_name, "s  \u03A3 ", resource_name, "s = ", i - 1, "\n"))
-# 		b <- b + 1
+# 		bundles[[bundle_count]] <- blist
+# 		cat(paste0("Bundle ", bundle_count, " a ", current_bundle_size, " ", resource_name, "s  \u03A3 ", total - 1, "\n"))
+# 		bundle_count <- bundle_count + 1
 # 	}
 # 	fhir_bundle_list(bundles)
-#
 # }
-#
-get_resources_by_ids_fix_bundle_size <- function(endpoint, resource_name, ids, bundle_size = 7, seed = as.double(Sys.time())) {
-	# cnt <- get_resources_count(endpoint = endpoint, resource_name = resource_name)
-	# if(cnt < sample_size) stop("The sample must be smaller or equal than the population size.")
-	# ids <- get_resources_ids(endpoint = endpoint, resource_name = resource_name)
-	# set.seed(seed = seed)
-	# ids <- try(sample(ids, sample_size, replace = F))
-	# #	ids <- sort(ids)
-	# if(inherits(ids, "try-error"))  stop("The sample must be smaller or equal than the population size.")
 
-	bundles <- list()
-	total <- 1
-	bundle_count <- 1
-	while(0 < length(ids)) {
-		blist <- xml2::xml_new_root("Bundle")
-		end <- min(c(bundle_size, length(ids)))
-		current_bundle_size <- 0
-		while(current_bundle_size < end) {
-			ids_ <- collect_indices_for_request(ids = ids, max_ids = end - current_bundle_size, 1200)
-			url_ <- paste0(paste_paths(endpoint, resource_name), "?_id=", ids_$str)
-			bnd_ <- fhir_search(request = url_, verbose = 0, log_errors = "tmp/log_get_resources_fix_bundlesize.txt")
-			for(bn in bnd_) {
-				entries <- xml2::xml_find_all(bn, paste0("entry[./resource/", resource_name,"]"))
-				for(entry in entries) xml2::xml_add_child(blist, entry)
-			}
-			total <- total + ids_$n
-			current_bundle_size <- current_bundle_size + ids_$n
-			ids <- ids[-seq_len(ids_$n)]
-		}
-		bundles[[bundle_count]] <- blist
-		cat(paste0("Bundle ", bundle_count, " a ", current_bundle_size, " ", resource_name, "s  \u03A3 ", total - 1, "\n"))
-		bundle_count <- bundle_count + 1
-	}
-	fhir_bundle_list(bundles)
-}
-
-sample_resources_fix_bundle_size <- function(endpoint, resource_name, sample_size = 20, bundle_size = 7, seed = as.double(Sys.time())) {
-	cnt <- get_resources_count(endpoint = endpoint, resource_name = resource_name)
-	if(cnt < sample_size) stop("The sample must be smaller or equal than the population size.")
-	ids <- get_resources_ids(endpoint = endpoint, resource_name = resource_name)
-	set.seed(seed = seed)
-	ids <- try(sample(ids, sample_size, replace = F))
-	#	ids <- sort(ids)
-	if(inherits(ids, "try-error"))  stop("The sample must be smaller or equal than the population size.")
-
-	get_resources_by_ids_fix_bundle_size(endpoint = endpoint, resource_name = resource_name, ids = ids, bundle_size = bundle_size, seed = seed)
-}
-
-get_resources_by_ids <- function(endpoint, resource_name, ids, seed = as.double(Sys.time())) {
+get_resources_by_ids <- function(endpoint, resource_name, ids, seed = as.double(Sys.time()), verbose = 1) {
 	bundles <- list()
 	total <- 1
 	bundle_count <- 1
 	while(0 < length(ids)) {
 		ids_ <- collect_indices_for_request(ids = ids, max_ids = length(ids))
 		url_ <- paste0(paste_paths(endpoint, resource_name), "?_id=", ids_$str)
-		bnd_ <- fhir_search(request = url_, verbose = 0, log_errors = "tmp/log_get_resources.txt")
+		bnd_ <- fhir_search(request = url_, verbose = 0)
 		total <- total + ids_$n
 		ids <- ids[-seq_len(ids_$n)]
 		bundles <- c(bundles, bnd_)
@@ -131,14 +213,61 @@ get_resources_by_ids <- function(endpoint, resource_name, ids, seed = as.double(
 	fhir_bundle_list(bundles)
 }
 
-sample_resources <- function(endpoint, resource_name, sample_size = 20, seed = as.double(Sys.time())) {
-	cnt <- get_resources_count(endpoint = endpoint, resource_name = resource_name)
+get_resources_by_ids_post <- function(endpoint, resource_name, ids, seed = as.double(Sys.time()), verbose = 1) {
+	fhir_search(
+		fhir_url(
+			url = endpoint,
+			resource = resource_name,
+			url_enc = T
+		),
+		fhir_body(
+			content = list(
+				"_id" = paste0(ids, collapse = ","),
+				"_count" = "500"
+			),
+			type = "application/x-www-form-urlencoded"
+		),
+		verbose = verbose
+	)
+	# bundles <- list()
+	# total <- 1
+	# bundle_count <- 1
+	# while(0 < length(ids)) {
+	# 	ids_ <- collect_indices_for_request(ids = ids, max_ids = length(ids))
+	# 	url_ <- paste0(paste_paths(endpoint, resource_name), "?_id=", ids_$str)
+	# 	bnd_ <- fhir_search(request = url_, verbose = 0)
+	# 	total <- total + ids_$n
+	# 	ids <- ids[-seq_len(ids_$n)]
+	# 	bundles <- c(bundles, bnd_)
+	# 	cat(paste0("Bundle ", bundle_count, " a ", ids_$n, " ", resource_name, "s  \u03A3 ", total - 1, "\n"))
+	# 	bundle_count <- bundle_count + 1
+	# }
+	# fhir_bundle_list(bundles)
+}
+
+# sample_resources_fix_bundle_size <- function(endpoint, resource_name, sample_size = 20, bundle_size = 7, seed = as.double(Sys.time())) {
+# 	cnt <- get_resources_count(endpoint = endpoint, resource_name = resource_name)
+# 	if(cnt < sample_size) stop("The sample must be smaller or equal than the population size.")
+# 	ids <- get_resources_ids(endpoint = endpoint, resource_name = resource_name, verbose = 1)
+# 	set.seed(seed = seed)
+# 	ids <- try(sample(ids, sample_size, replace = F))
+# 	#	ids <- sort(ids)
+# 	if(inherits(ids, "try-error"))  stop("The sample must be smaller or equal than the population size.")
+#
+# 	get_resources_by_ids_fix_bundle_size(endpoint = endpoint, resource_name = resource_name, ids = ids, bundle_size = bundle_size, seed = seed)
+# }
+
+sample_resources <- function(endpoint, resource_name, parameters = parameters, sample_size = 20, seed = as.double(Sys.time()), verbose = 1) {
+	cnt <- get_resources_count(endpoint = endpoint, resource_name = resource_name, parameters = parameters, verbose = 1)
 	if(cnt < sample_size) stop("The sample must be smaller or equal than the population size.")
-	ids <- get_resources_ids(endpoint = endpoint, resource_name = resource_name)
+	ids <- get_resources_ids(endpoint = endpoint, resource_name = resource_name, parameters = parameters, verbose = verbose)
 	set.seed(seed = seed)
 	ids <- try(sample(ids, sample_size, replace = F))
 	#	ids <- sort(ids)
 	if(inherits(ids, "try-error"))  stop("The sample must be smaller or equal than the population size.")
-	get_resources_by_ids(endpoint = endpoint, resource_name = resource_name, ids = ids, seed = seed)
+	bundles <- try(get_resources_by_ids_post(endpoint = endpoint, resource_name = resource_name, ids = ids, seed = seed, verbose = verbose))
+	if(inherits(bundles, "try-error")) {
+		bundles <- get_resources_by_ids(endpoint = endpoint, resource_name = resource_name, ids = ids, seed = seed, verbose = verbose)
+	}
+	bundles
 }
-
--- sample_test.R
+++ sample_test.R
@@ -6,19 +6,121 @@ for(p in c(
 )
 )library(p, character.only = T)
 
+N <- 10
+
+(ids <- sapply(1:N, function(x)paste0(sample(c(LETTERS[1:3], letters[4:10], 0:100), sample(1:10,1)), collapse = "-")))
+order(ids)
+sort(ids)
+order_correctly_strings_with_numbers(ids)
+sort_correctly_strings_with_numbers(s=ids)
 endpoints <- list(
 #	azure = "http://nprogram.azurewebsites.net/",
+#	polar = "https://mii-agiop-polar.life.uni-leipzig.de/blaze",
 	hapi = "https://hapi.fhir.org/baseR4",
 	agiop = "https://mii-agiop-3p.life.uni-leipzig.de/fhir",
 	blaze = "https://mii-agiop-3p.life.uni-leipzig.de/blaze",
 	vonk  = "https://vonk.fire.ly/R4"
 )
 
-endpoint <- endpoints$agiop
+cap_stats <- lapply(
+	endpoints,
+	fhir_capability_statement
+)
+
+metas <- rbindlist(
+	fill = T,
+	lapply(
+		cap_stats,
+		function(x) {
+		   data.table(x$Meta)
+		}
+	)
+)
+metas$endpoint <- names(endpoints)
+
+
+
+rests <- rbindlist(
+	fill = T,
+	lapply(
+		cap_stats,
+		function(x) {
+			data.table(x$Rest)
+		}
+	)
+)
+rests$endpoint <- names(endpoints)
+
+endpoint <- endpoints$hapi
 resource_name <- "Patient"
-sep <- " <~> "
-brackets <- c("[:", ":]")
-bundle_size <- 5
+sep <- " <> "
+brackets <- NULL#c("<|", "|>")
+bundle_size <- 500
+parameters <- c(
+	gender = "female",
+	name   = "Simpson"
+)
+
+bundles <- sample_resources(endpoint = endpoint, resource_name = resource_name, parameters = parameters, sample_size = 2000, verbose = 2)
+dt <- fhir_crack(bundles, design = fhir_table_description(resource_name))
+dt
+
+
+
+
+
+
+
+
+ids <- get_resources_ids(
+	endpoint = endpoint,
+	resource_name = resource_name,
+	parameters = parameters,
+	verbose = 2
+)
+
+bundles <- get_resources_by_ids_post(
+	endpoint = endpoint,
+	resource_name = resource_name,
+	ids = paste0(ids, collapse = ","),
+	seed = 1,
+	verbose = 2
+)
+
+dt <- fhir_crack(bundles, design = fhir_table_description(resource_name))
+
+body <- fhir_body(
+	content = list(
+		"_id" = paste0(ids, collapse = ",")
+	)
+)
+
+request <- fhir_url(
+	url = endpoint,
+	resource = resource_name
+)
+
+bundles <- fhir_search(
+	request = request,
+	body = body
+)
+
+dt <- fhir_crack(
+	bundles = bundles,
+	design = fhir_table_description(
+		resource = resource_name,
+		style = fhir_style(
+			sep = sep,
+			brackets = brackets,
+			rm_empty_cols = T
+		)
+	),
+	verbose = 2,
+	data.table = T
+)
+dt
+
+dt[dt$id %in% ids,]
 
 # bundles <- fhir_search(paste0(paste_paths(endpoint, resource_name), "?_count=", bundle_size), max_bundles = 3, verbose = 2)
 # tables  <- fhir_crack(bundles, fhir_table_description(res_name, style = fhir_style(sep, brackets)))
@@ -45,50 +147,130 @@ bundle_size <- 5
 
 
 
+(endpoint <- endpoints$blaze)
+get_resources_count(endpoint, "Patient", list("gender" = "male"))
+get_resources_count(endpoint, "Patient", c("gender" = "male"))
+get_resources_count(endpoint, "Patient", "gender=male")
+get_resources_count(endpoint, "Patient")
 
-get_resource_count(endpoints$hapi, "Patient")
-get_resource_count(endpoints$hapi, "Observation")
-get_resource_count(endpoints$hapi, "Observation")
-get_resource_count(endpoints$vonk, "Patient")
-#get_resource_count(endpoint = endpoints$azure, "Observation")
-
-(ids_obs_agiop <- get_resource_ids(endpoint = endpoints$agiop, resource_name = "Observation"))
-(ids_obs_blaze <- get_resource_ids(endpoint = endpoints$blaze, resource_name = "Observation"))
-(ids_obs_hapi  <- get_resource_ids(endpoint = endpoints$hapi, resource_name = "Observation")) #hapi schickt nur knapp 12500 ids
-(ids_obs_vonk  <- get_resource_ids(endpoint = endpoints$vonk, resource_name = "Observation")) #hapi schickt nur knapp 12500 ids
-#(ids_obs_azure <- get_resource_ids(endpoint = endpoints$azure, resource_name = "Observation")) #
+(m <- get_resources_count(endpoint, "Patient", c("gender" = "male")))
+(f <- get_resources_count(endpoint, "Patient", c("gender" = "female")))
+(a <- get_resources_count(endpoint = endpoint, resource_name = "Patient", parameters = "", verbose = 2))
+a - m - f
 
 resource_name <- "Patient"
-fhir_crack(get_samples(endpoint = endpoints$agiop, resource_name, sample_size = 10, bundle_size = 5, seed = 2), fhir_table_description(resource_name))
-resource_name <- "Patient"
-fhir_crack(get_samples(endpoints$vonk, resource_name, 100, 10, 2), fhir_table_description(resource_name))
-resource_name <- "Observation"
+parameters <- list(
+	male = c("gender" = "male"),
+	female = c("gender" = "female"),
+	all = ""
+)
+
+for(endpoint in endpoints) {
+	cat(endpoint, "\n")
+	cat("males:              ", m <- get_resources_count(endpoint, resource_name = resource_name, parameters = parameters$male), "\n")
+	cat("females:            ", f <- get_resources_count(endpoint, resource_name = resource_name, parameters = parameters$female), "\n")
+	cat("all:                ", a <- get_resources_count(endpoint, resource_name = resource_name, parameters = parameters$all), "\n")
+	cat("all - male - female:", a - m - f, "\n\n")
+}
 
 resource_name <- "Observation"
+parameters <- list(
+	male = c("patient.gender" = "male"),
+	female = c("patient.gender" = "female"),
+	all = ""
+)
+
+for(endpoint in endpoints) {
+	cat(endpoint, "\n")
+	cat("males:              ", m <- get_resources_count(endpoint, resource_name, parameters$male), "\n")
+	cat("females:            ", f <- get_resources_count(endpoint, resource_name, parameters$female), "\n")
+	cat("all:                ", a <- get_resources_count(endpoint, resource_name, parameters$all), "\n")
+	cat("all - male - female:", a - m - f, "\n\n")
+}
+
+
+rn <- "Observation"
+(ids_obs_agiop <- sort_correctly_strings_with_numbers(get_resources_ids(endpoint = endpoints$agiop, resource_name = rn)))
+(ids_obs_blaze <- sort_correctly_strings_with_numbers(get_resources_ids(endpoint = endpoints$blaze, resource_name = rn))) #blaze laed die komplette resource herunter, kann kein _elements
+(ids_obs_hapi  <- sort_correctly_strings_with_numbers(get_resources_ids(endpoint = endpoints$hapi, resource_name = rn, verbose = 2)))
+(ids_obs_vonk  <- sort_correctly_strings_with_numbers(get_resources_ids(endpoint = endpoints$vonk, resource_name = rn, verbose = 2))) #hapi schickt nur knapp 12500 ids
+#(ids_obs_azure <- get_resource_ids(endpoint = endpoints$azure, resource_name = rn)) #
+
+resource_name <- "Patient"
+patients_bundles1 <- sample_resources(
+	resource_name = resource_name,
+	endpoint = endpoints$vonk,
+	sample_size = 500,
+	seed = 1
+)
+
+patients_bundles2 <- sample_resources_fix_bundle_size(
+	resource_name = resource_name,
+	endpoint = endpoints$vonk,
+	sample_size = 500,
+	bundle_size = 250,
+	seed = 1
+)
+
+(d1 <- fhir_crack(patients_bundles1, fhir_table_description(resource_name)))
+(d2 <- fhir_crack(patients_bundles2, fhir_table_description(resource_name)))
+
+d1 <- d1[order(d1$id),]
+d2 <- d2[order(d2$id),]
+
+identical(d1, d2)
+identical(names(d1), names(d2))
+all(
+	sapply(
+		names(d1),
+		function(n){
+			identical(d1[[n]], d2[[n]])
+		}
+	)
+)
+
+resource_name <- "Patient"
+fhir_crack(sample_resources_fix_bundle_size(endpoints$vonk, resource_name, 100, 10, 2), fhir_table_description(resource_name))
+
 start <- Sys.time()
-s <- get_samples(
+s <- sample_resources_fix_bundle_size(
 	endpoint = endpoints$hapi,
 	resource_name = resource_name,
 	sample_size = 10000,
-	bundle_size = 500,
+	bundle_size = 1000,
 	seed = 1000)
 Sys.time() - start
 
 start <- Sys.time()
-d <- fhir_crack(s, fhir_table_description(resource_name))
+s <- sample_resources(
+	endpoint = endpoints$hapi,
+	resource_name = resource_name,
+	sample_size = 10000,
+	seed = 1000)
 Sys.time() - start
 
-
-d2 <- fhir_crack(get_samples(endpoints$hapi, resource_name, 1000, 1000), fhir_table_description(resource_name))
+d1 <- fhir_crack(sample_resources_fix_bundle_size(endpoints$vonk, resource_name, 100, 1000, 1), fhir_table_description(resource_name))
+d2 <- fhir_crack(sample_resources(endpoints$vonk, resource_name, 100, 1), fhir_table_description(resource_name))
+d1 <- d1[order(d1$id),]
+d2 <- d2[order(d2$id),]
 identical(d1, d2)
+all(
+	sapply(
+		names(d1),
+		function(n) {
+			identical(d1[[n]], d2[[n]])
+		}
+	)
+)
 
-d1 <- fhir_crack(get_samples(endpoints$vonk, resource_name, 100, 1000), fhir_table_description(resource_name))
-d2 <- fhir_crack(get_samples(endpoints$vonk, resource_name, 100, 1000), fhir_table_description(resource_name))
-identical(d1, d2)
-
-d1 <- fhir_crack(get_samples(endpoints$vonk, resource_name, 100, 1001), fhir_table_description(resource_name))
-d2 <- fhir_crack(get_samples(endpoints$vonk, resource_name, 100, 1000), fhir_table_description(resource_name))
+d1 <- fhir_crack(sample_resources_fix_bundle_size(endpoints$vonk, resource_name, 500, 1000, 1), fhir_table_description(resource_name))
+d2 <- fhir_crack(sample_resources_fix_bundle_size(endpoints$vonk, resource_name, 500, 1000, 2), fhir_table_description(resource_name))
 identical(d1, d2)
 
 d1
 d2
+
+sum(d1$id %in% d2$id)
+sum(d2$id %in% d1$id)
+
+
